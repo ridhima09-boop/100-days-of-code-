d4fad11ff113ba97a4a11265da2356ab08769a07
ridhima09-boop <ridhimasuneja9@gmail.com>
2025-12-08 00:00:37 +0530

    Create 11.c

---
+++ day1-10/11.c
@@ -0,0 +1,20 @@
+// Write a program to input an integer and check whether it is even or odd using if–else.
+#include <stdio.h>
+void nature(int n);
+int main(){
+    int x;
+    printf("ENTER THE INTEGER:");
+    scanf("%d",&x);
+    nature(x);
+    return 0;
+    
+}
+void nature(int n){
+    if(n%2==0){
+        printf("%d IS EVEN",n);
+    }
+    
+    else{
+        printf("%d IS ODD",n);
+    }
+}

0caa8fb500a260fd7ec3183bf71517f74ab5b84f
ridhima09-boop <ridhimasuneja9@gmail.com>
2025-12-08 00:01:02 +0530

    Create 12.c

---
+++ day1-10/12.c
@@ -0,0 +1,26 @@
+// Write a program to input an integer and check whether it is positive, negative or zero using nested if–else.
+#include <stdio.h>
+void nature(int n);
+int main(){
+    int x;
+    printf("ENTER YOUR INTEGER:");
+    scanf("%d",&x);
+    nature(x);
+    return 0;
+
+}
+void nature(int n){
+    if(n>=0){
+        if(n==0){
+        printf("%d IS ZERO",n);
+    }
+        else{
+         printf("%d IS POSITIVE",n);
+        }
+    }
+    else{
+        printf("%d IS NEGATIVE",n);
+
+    }
+
+}

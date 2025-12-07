954c9b2b291b3cfa0415214b42bf837845d26fbe
ridhima09-boop <ridhimasuneja9@gmail.com>
2025-12-08 01:45:49 +0530

    Create 29.c

---
+++ day11-20/29.c
@@ -0,0 +1,20 @@
+//Write a program to calculate the factorial of a number.
+#include <stdio.h>
+int fact(int n);
+int main(){
+    int x;
+    printf("ENETER THE NUMBER WHOSE FACTORIAL YOU WANT:");
+    scanf("%d",&x);
+    printf("FACTORIAL OF THE ENTERED NUMBER IS %d",fact(x));
+    return 0;
+
+}
+int fact(int n){
+    if(n==1 || n==0){
+        return 1;
+    }
+    int factNm1=fact(n-1);
+    int factn=factNm1*n;
+    return factn;
+
+}
